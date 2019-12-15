3ea8b94c48c216b60b9d226f5befb274f495fbbd
Nikita Basargin <nbasargin@web.de>
2019-12-15 14:47:55 +0100

    set up full screen canvas and render loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     template: `
-        <div style="text-align:center">
-            <h1>
-                Welcome to {{title}}!
-            </h1>
+        <div #wrapper class="full-size">
+            <canvas #canvas></canvas>
         </div>
     `,
-    styles: []
+    styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-    title = 'point-cloud-rendering';
+export class AppComponent implements AfterViewInit, OnDestroy {
+
+    @ViewChild('canvas', {static: true}) canvasRef: ElementRef<HTMLCanvasElement>;
+    @ViewChild('wrapper', {static: true}) wrapperRef: ElementRef<HTMLDivElement>;
+
+    private animationRequest;
+
+    ngAfterViewInit(): void {
+        this.renderLoop();
+    }
+
+    ngOnDestroy(): void {
+        cancelAnimationFrame(this.animationRequest);
+    }
+
+    renderLoop() {
+        this.animationRequest = requestAnimationFrame(() => this.renderLoop());
+        this.checkCanvasSize();
+    }
+
+    checkCanvasSize() {
+        const c = this.canvasRef.nativeElement;
+        const w = this.wrapperRef.nativeElement;
+        const bb = w.getBoundingClientRect();
+        if (c.width !== bb.width || c.height !== bb.height) {
+            c.width = bb.width;
+            c.height = bb.height;
+            console.debug(`resizing canvas to ${bb.width} x ${bb.height}`);
+        }
+    }
 }
