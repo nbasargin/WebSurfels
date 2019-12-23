2f7058d5b056a5b538a66e66c7ddaccb1f4e6281
Nikita Basargin <nbasargin@web.de>
2019-12-23 21:10:56 +0100

    passing normals to shaders

--- src/point-cloud-rendering/renderer/renderer.ts
+++ src/point-cloud-rendering/renderer/renderer.ts
@@ -13,6 +13,7 @@ export class Renderer {
     private readonly attributes: {
         pos: GLint,
         color: GLint,
+        normal: GLint,
     };
 
     private readonly uniforms: {
@@ -23,6 +24,7 @@ export class Renderer {
     private readonly buffers: {
         pos: WebGLBuffer,
         color: WebGLBuffer,
+        normal: WebGLBuffer,
     };
 
     private readonly numPoints = 100000;
@@ -42,6 +44,7 @@ export class Renderer {
         this.attributes = {
             pos: this.gl.getAttribLocation(this.program, 'pos'),
             color: this.gl.getAttribLocation(this.program, 'color'),
+            normal: this.gl.getAttribLocation(this.program, 'normal'),
         };
 
         this.uniforms = {
@@ -52,6 +55,7 @@ export class Renderer {
         this.buffers = {
             pos: this.gl.createBuffer() as WebGLBuffer,
             color: this.gl.createBuffer() as WebGLBuffer,
+            normal: this.gl.createBuffer() as WebGLBuffer,
         };
 
         this.projectionMatrix = mat4.create();
@@ -64,8 +68,11 @@ export class Renderer {
 
         this.setBufferData(this.buffers.pos, data.positions);
         this.setBufferData(this.buffers.color, data.colors);
+        this.setBufferData(this.buffers.normal, data.normals);
 
-        this.setBufferAttrib();
+        this.enableBuffer3f(this.buffers.pos, this.attributes.pos);
+        this.enableBuffer3f(this.buffers.color, this.attributes.color);
+        this.enableBuffer3f(this.buffers.normal, this.attributes.normal);
 
         this.gl.useProgram(this.program);
         this.gl.enable(this.gl.DEPTH_TEST);
@@ -100,21 +107,16 @@ export class Renderer {
         this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
     }
 
-
-    private setBufferAttrib() {
+    private enableBuffer3f(buffer: WebGLBuffer, attrib: GLint) {
         const numComponents = 3;
         const type = this.gl.FLOAT;
         const normalize = false;
         const stride = 0;
         const offset = 0;
 
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.pos);
-        this.gl.vertexAttribPointer(this.attributes.pos, numComponents, type, normalize, stride, offset);
-        this.gl.enableVertexAttribArray(this.attributes.pos);
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.color);
-        this.gl.vertexAttribPointer(this.attributes.color, numComponents, type, normalize, stride, offset);
-        this.gl.enableVertexAttribArray(this.attributes.color);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.vertexAttribPointer(attrib, numComponents, type, normalize, stride, offset);
+        this.gl.enableVertexAttribArray(attrib);
     }
 
     private initShaderProgram(vsSource: string, fsSource: string): WebGLProgram {
--- src/point-cloud-rendering/renderer/shaders.ts
+++ src/point-cloud-rendering/renderer/shaders.ts
@@ -5,17 +5,20 @@
 export const vertexShader = `
     attribute vec3 pos;
     attribute vec3 color;
+    attribute vec3 normal; 
     
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
     
     varying vec3 v_color;
+    varying vec3 v_normal;
     
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(pos, 1); 
       // mix(from, to, progress)
       gl_PointSize = 2.0;
       v_color = color;
+      v_normal = normal;
     }
 `;
 
@@ -24,6 +27,7 @@ export const fragmentShader = `
     precision highp float;
 
     varying vec3 v_color;
+    varying vec3 v_normal;
 
     void main() {
       gl_FragColor = vec4(v_color, 1.0);
