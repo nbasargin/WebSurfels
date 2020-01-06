25578c749fac730018eb2d1a23ec71e7848cfca4
Nikita Basargin <nbasargin@web.de>
2020-01-06 01:17:02 +0100

    new test geometry generator

--- src/point-cloud-rendering/data/point-cloud-data-generator.ts
+++ src/point-cloud-rendering/data/point-cloud-data-generator.ts
@@ -10,19 +10,19 @@ export class PointCloudDataGenerator {
         data.normals = new Float32Array(pointNumber * 3);
         for (let i = 0; i < pointNumber; i++) {
             const offset = i * 3;
-            const randomPoint = PointCloudDataGenerator.randomPointOnSphere(); // randomPointOnCircle();
+            const randomPoint = PointCloudDataGenerator.randomPointOnSphere();
 
             data.positions[offset] = randomPoint[0];
             data.positions[offset + 1] = randomPoint[1];
             data.positions[offset + 2] = randomPoint[2];
 
-            data.colors[offset] = Math.random() * 0.5 + 0.5; // Math.max(0, randomPoint[0]);
-            data.colors[offset + 1] = Math.random() * 0.5 + 0.5; // Math.max(0, randomPoint[1]);
-            data.colors[offset + 2] = Math.random() * 0.5 + 0.5; // Math.max(0, randomPoint[2]);
+            data.colors[offset] = randomPoint[3];
+            data.colors[offset + 1] = randomPoint[4];
+            data.colors[offset + 2] = randomPoint[5];
 
-            data.normals[offset] = randomPoint[0];
-            data.normals[offset + 1] = randomPoint[1];
-            data.normals[offset + 2] = randomPoint[2];
+            data.normals[offset] = randomPoint[6];
+            data.normals[offset + 1] = randomPoint[7];
+            data.normals[offset + 2] = randomPoint[8];
         }
 
         return data;
@@ -33,22 +33,29 @@ export class PointCloudDataGenerator {
      * Uniform distribution.
      * http://corysimon.github.io/articles/uniformdistn-on-sphere/
      */
-    private static randomPointOnSphere(): [number, number, number] {
+    private static randomPointOnSphere(): [number, number, number, number, number, number, number, number, number] {
         const theta = 2 * Math.PI * Math.random();
         const phi = Math.acos(1 - 2 * Math.random());
         const x = Math.sin(phi) * Math.cos(theta);
         const y = Math.sin(phi) * Math.sin(theta);
         const z = Math.cos(phi);
-        return [x, y, z];
+        return [x, y, z, Math.max(0, x), Math.max(0, y), Math.max(0, z), x, y, z];
     }
 
-    private static randomPointOnCircle(): [number, number, number] {
-        const angle = Math.random() * Math.PI * 2;
-        const x = Math.cos(angle);
-        const y = Math.sin(angle);
-        const z = 0;
-        return [x, y, z];
-    }
+    private static randomPointOnQuads(): [number, number, number, number, number, number, number, number, number] {
+        const x = Math.random() * 2 - 1;
+        const y = Math.random() * 2 - 1;
+        const z = Math.floor(Math.random() * 5) + y;
 
+        const r = Math.random() * 0.3 + 0.7 * (1 - z / 4);
+        const g = Math.random() * 0.3 + 0.7 * (1 - z / 4);
+        const b = Math.random() * 0.3 + 0.7 * (1 - z / 4);
+
+        const nx = 0;
+        const ny = Math.sqrt(2) / 2;
+        const nz = Math.sqrt(2) / 2;
+
+        return [x, y, -z, r, g, b, nx, ny, nz];
+    }
 
 }
