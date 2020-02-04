87250eee5e850e7c3e144d75f8c425c77019a848
Nikita Basargin <nbasargin@web.de>
2020-02-04 18:28:02 +0100

    add points to inner node

--- src/point-cloud-rendering/octree2/inner-node.ts
+++ src/point-cloud-rendering/octree2/inner-node.ts
@@ -14,10 +14,13 @@ export class InnerNode implements OctreeNode {
     }
 
     addPoint(data: PointCloudData, pointIndex: number) {
+        const x = data.positions[pointIndex] > this.nodeInfo.centerX ? 1 : 0;
+        const y = data.positions[pointIndex + 1] > this.nodeInfo.centerX ? 1 : 0;
+        const z = data.positions[pointIndex + 2] > this.nodeInfo.centerX ? 1 : 0;
+        this.children[x + y * 2 + z * 4].addPoint(data, pointIndex);
     }
 
     computeLOD(): LodNode {
         return undefined as any;
     }
-
 }
