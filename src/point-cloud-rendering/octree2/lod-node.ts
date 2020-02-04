c97900ea1844102c27c6a20e2788fd045fe71347
Nikita Basargin <nbasargin@web.de>
2020-02-04 23:10:58 +0100

    define LOD node

--- src/point-cloud-rendering/octree2/lod-node.ts
+++ src/point-cloud-rendering/octree2/lod-node.ts
@@ -2,6 +2,19 @@
  * Level of detail node optimized for rendering.
  * Does not allow adding new points.
  */
-export class LodNode {
+import { PointCloudData } from '../data/point-cloud-data';
+import { OctreeNodeInfo } from './octree-node';
+
+export interface LodNode extends PointCloudData {
+
+    nodeInfo: OctreeNodeInfo;
+
+    positions: Float32Array;
+    sizes: Float32Array;
+    colors: Float32Array;
+    normals: Float32Array;
+    weights: Float32Array;
+
+    children: Array<LodNode>;
 
 }
