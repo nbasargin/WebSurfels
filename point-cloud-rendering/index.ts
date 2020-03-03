c20bb33c16ec117ed2cbea11fe402a3dd7907bbc
Nikita Basargin <nbasargin@web.de>
2020-03-03 15:46:32 +0100

    moved code to src

--- point-cloud-rendering/index.ts
+++ point-cloud-rendering/index.ts
@@ -1,26 +1,26 @@
 export * from 'gl-matrix';
 
-export * from './renderer2/renderer2';
-export * from './renderer2/view-direction';
-export * from './renderer2/frustum';
-export * from './renderer2/renderer-node';
+export * from './src/renderer2/renderer2';
+export * from './src/renderer2/view-direction';
+export * from './src/renderer2/frustum';
+export * from './src/renderer2/renderer-node';
 
-export * from './street-view/point-cloud-factory';
-export * from './street-view/panorama-loader';
-export * from './street-view/depth-data';
+export * from './src/street-view/point-cloud-factory';
+export * from './src/street-view/panorama-loader';
+export * from './src/street-view/depth-data';
 
-export * from './utils/geometry';
-export * from './utils/animated-camera';
-export * from './utils/fps-counter';
-export * from './utils/timing';
+export * from './src/utils/geometry';
+export * from './src/utils/animated-camera';
+export * from './src/utils/fps-counter';
+export * from './src/utils/timing';
 
-export * from './level-of-detail/lod-builder';
-export * from './level-of-detail/lod-tree';
-export * from './level-of-detail/octree-lod-buider/octree-lod-builder';
+export * from './src/level-of-detail/lod-builder';
+export * from './src/level-of-detail/lod-tree';
+export * from './src/level-of-detail/octree-lod-buider/octree-lod-builder';
 
-export * from './data/point-cloud-data';
-export * from './data/point-cloud-data-generator';
-export * from './data/stanford-dragon-loader';
+export * from './src/data/point-cloud-data';
+export * from './src/data/point-cloud-data-generator';
+export * from './src/data/stanford-dragon-loader';
 
-export * from './culling-tree/culling-tree';
+export * from './src/culling-tree/culling-tree';
 
