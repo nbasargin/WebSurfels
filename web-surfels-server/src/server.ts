bcace7ea600cd068d6fe03dd00cb307af2033c02
Nikita Basargin <nbasargin@web.de>
2020-03-07 23:19:01 +0100

    loading dragon ply on server

--- web-surfels-server/src/server.ts
+++ web-surfels-server/src/server.ts
@@ -1,19 +1,30 @@
-import { Geometry, OctreeLodBuilder, LodNode, PointCloudDataGenerator, Timing, LodBinary } from 'web-surfels';
+import { Geometry, OctreeLodBuilder, LodNode, Timing, LodBinary, PointCloudData } from 'web-surfels';
 import { FileIO } from './file-io/file-io';
+import { PLYLoader } from '@loaders.gl/ply';
+import { parse } from '@loaders.gl/core';
 
-console.log(Timing.measure(), 'starting');
-
-const data = PointCloudDataGenerator.generateSphere(1000000, 0.01, true);
-console.log(Timing.measure(), 'generated sphere');
-
-const bb = Geometry.getBoundingBox(data.positions);
-const octree = new OctreeLodBuilder(bb, 128, 10);
-octree.addData(data);
-console.log(Timing.measure(), 'octree created');
-
-const lod = octree.buildLod();
-console.log(Timing.measure(), 'lod computed'); // bounding sphere is lod.boundingSphere
-
+async function loadData(): Promise<PointCloudData> {
+    const castleFile = await FileIO.readFile('../point-clouds/stanford_dragon.ply');
+    const rawData = await parse(castleFile, PLYLoader);
+    const sizes = new Float32Array(Math.floor(rawData.attributes.POSITION.value.length / 3));
+    sizes.fill(0.03);
+    const data: PointCloudData = {
+        positions: rawData.attributes.POSITION.value,
+        sizes: sizes,
+        normals: rawData.attributes.NORMAL.value,
+        colors: new Float32Array(rawData.attributes.COLOR_0.value),
+    };
+    for (let i = 0; i < data.positions.length; i++) {
+        data.positions[i] *= 20.0;
+    }
+    for (let i = 1; i < data.positions.length; i+=3) {
+        data.positions[i] -= 2.5;
+    }
+    for (let i = 0; i < data.colors.length; i++) {
+        data.colors[i] /= 255.0;
+    }
+    return data;
+}
 
 // write lod to files
 async function writeLodTreeToFiles(lod: LodNode, folderPath: string) {
@@ -29,9 +40,24 @@ async function writeLodNode(node: LodNode, folderPath: string, isRootNode: boole
     }
 }
 
-const folderPath = './lod/';
-writeLodTreeToFiles(lod, folderPath).then(() => {
-    console.log(Timing.measure(), 'done writing lod');
-}).catch(err => {
-    console.log('error writing lod', err);
+console.log(Timing.measure(), 'starting');
+
+loadData().then(data => {
+    console.log(Timing.measure(), 'raw data loaded');
+
+    const bb = Geometry.getBoundingBox(data.positions);
+    const octree = new OctreeLodBuilder(bb, 32, 10);
+    octree.addData(data);
+    console.log(Timing.measure(), 'octree created');
+
+    const lod = octree.buildLod();
+    console.log(Timing.measure(), 'lod computed'); // bounding sphere is lod.boundingSphere
+
+    const folderPath = './lod/';
+    writeLodTreeToFiles(lod, folderPath).then(() => {
+        console.log(Timing.measure(), 'done writing lod');
+    }).catch(err => {
+        console.log('error writing lod', err);
+    });
+
 });
