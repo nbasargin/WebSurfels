b8fdbd2d7b3345ca43f08200b9d71413dbfbad46
Nikita Basargin <nbasargin@web.de>
2020-03-19 13:13:28 +0100

    more detailed type for street view panoramas

--- web-surfels-lib/src/street-view/panorama-loader.ts
+++ web-surfels-lib/src/street-view/panorama-loader.ts
@@ -5,6 +5,7 @@ export interface StreetViewResponse {
         [k: string]: string,
     };
     Links: Array<{
+        yawDeg: string,
         panoId: string,
         description?: string,
     }>;
@@ -16,7 +17,12 @@ export interface StreetViewResponse {
         original_lat: string,
         original_lng: string,
     };
-    Projection: any;
+    Projection: {
+        projection_type: 'spherical' | string,
+        pano_yaw_deg: string,
+        tilt_yaw_deg: string,
+        tilt_pitch_deg: string,
+    };
     model: {
         depth_map: string
     }
