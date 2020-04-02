7b1e297e30c92dba18a8757ea605d414cd7518ff
Nikita Basargin <nbasargin@web.de>
2020-04-02 21:30:24 +0200

    s for snowden

--- web-surfels-ui/src/lib/street-view/gsv-panorama-loader.ts
+++ web-surfels-ui/src/lib/street-view/gsv-panorama-loader.ts
@@ -100,20 +100,20 @@ export class GSVPanoramaLoader {
 
     // ID like 'GTKQkr3G-rRZQisDUMzUtg'
     public static async loadById(panoID: string): Promise<GSVApiResponse> {
-        const url = `http://maps.google.com/cbk?output=json&panoid=${panoID}&dm=1`;
+        const url = `https://maps.google.com/cbk?output=json&panoid=${panoID}&dm=1`;
         const response = await fetch(url);
         return response.json();
     }
 
     // Location like 40.762475, -73.974363
     private static async loadByLocation(lat: number, lng: number): Promise<GSVApiResponse> {
-        const url = `http://maps.google.com/cbk?output=json&ll=${lat},${lng}&dm=1`;
+        const url = `https://maps.google.com/cbk?output=json&ll=${lat},${lng}&dm=1`;
         const response = await fetch(url);
         return response.json();
     }
 
     private static async loadImage(panoID: string, zoom: number, x: number, y: number): Promise<ImageBitmap> {
-        const url = `http://maps.google.com/cbk?output=tile&panoid=${panoID}&zoom=${zoom}&x=${x}&y=${y}`;
+        const url = `https://maps.google.com/cbk?output=tile&panoid=${panoID}&zoom=${zoom}&x=${x}&y=${y}`;
         const response = await fetch(url);
         const blob = await response.blob();
         return createImageBitmap(blob);
