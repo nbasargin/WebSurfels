7cfde7a95871085c24c490a2b09de28321adbae8
Nikita Basargin <nbasargin@web.de>
2020-05-21 13:39:34 +0200

    moved single point construction into a separate function

--- web-surfels-ui/src/lib/data/street-view/street-view-loader.ts
+++ web-surfels-ui/src/lib/data/street-view/street-view-loader.ts
@@ -10,6 +10,13 @@ export interface StreetViewLoaderOptions {
     maxNonSkySplatSize: number;
 }
 
+type PointCloudOutputOutputBuffer = {
+    positions: Array<number>;
+    sizes: Array<number>;
+    colors: Array<number>;
+    normals: Array<number>;
+}
+
 export class StreetViewLoader {
 
     private canvas: HTMLCanvasElement;
@@ -93,64 +100,96 @@ export class StreetViewLoader {
      * @param depthData
      */
     private constructPointCloud(colorData: ImageBitmap, imageWidth: number, imageHeight: number, depthData: StreetViewDepthData): PointCloudData {
-        const splatScale = 0.02;
-
         this.ctx.drawImage(colorData, 0, 0);
         const pixels: Uint8ClampedArray = this.ctx.getImageData(0, 0, 512, 256).data;
 
-        const positions: Array<number> = [];
-        const sizes: Array<number> = [];
-        const colors: Array<number> = [];
-        const normals: Array<number> = [];
+        const output: PointCloudOutputOutputBuffer = {
+            positions: [],
+            sizes: [],
+            colors: [],
+            normals: [],
+        };
 
         const w = depthData.header.width;
         const h = depthData.header.height;
 
         for (let y = 0; y < h; y++) {
             for (let x = 0; x < w; x++) {
-                const planeID = depthData.indices[y * w + x];
-                const splatBelongsToSky = planeID === 0;
-                if (splatBelongsToSky && this.options.skyDistance <= 0) {
-                    continue; // point does not belong to a plane & sky should be discarded
-                }
-
-                const {px, py, pz} = this.getPointDirection(x, y, w, h);
-
-                let t: number;
-                let plane: { normal: [number, number, number], distance: number };
-                if (planeID !== 0) {
-                    plane = depthData.planes[planeID];
-                    t = plane.distance / (px * plane.normal[0] + py * plane.normal[1] + pz * plane.normal[2]);
-                } else {
-                    t = -this.options.skyDistance;
-                    plane = {normal: [-px, -py, -pz], distance: 0}
-                }
-
-                // size correction based on orientation:
-                // 1 / cos (angle between normal and ray to point) = 1 / dot product (normal, ray to point)
-                const orientationCorrection = 1 / Math.abs(px * plane.normal[0] + py * plane.normal[1] + pz * plane.normal[2]);
-                const distanceCorrection = Math.abs(t) + 0.001;
-                const size = splatScale * distanceCorrection * orientationCorrection;
-                if (!splatBelongsToSky && (size > this.options.maxNonSkySplatSize || size < this.options.minNonSkySplatSize)) {
-                    continue; // discard non-sky points that are too large
-                }
-
-                // color: map x and y to pixel position (depends on effective image size)
-                const {r, g, b} = this.getPointColor(x, y, w, h, imageWidth, imageHeight, pixels);
-
-                positions.push(px * t, py * t, pz * t);
-                sizes.push(size);
-                colors.push(r, g, b);
-                normals.push(-plane.normal[0], -plane.normal[1], -plane.normal[2]);
+                this.constructPoint(x, y, pixels, imageWidth, imageHeight, depthData, output)
             }
         }
 
         return {
-            positions: new Float32Array(positions),
-            sizes: new Float32Array(sizes),
-            colors: new Float32Array(colors),
-            normals: new Float32Array(normals),
+            positions: new Float32Array(output.positions),
+            sizes: new Float32Array(output.sizes),
+            colors: new Float32Array(output.colors),
+            normals: new Float32Array(output.normals),
         }
+
+    }
+
+    private constructPoint(
+        x: number,
+        y: number,
+        pixels: Uint8ClampedArray,
+        imageWidth: number,
+        imageHeight: number,
+        depthData: StreetViewDepthData,
+        output: PointCloudOutputOutputBuffer
+    ) {
+        const splatScale = 0.02;
+        const w = depthData.header.width;
+        const h = depthData.header.height;
+        const planeID = depthData.indices[y * w + x];
+        const splatBelongsToSky = planeID === 0;
+        if (splatBelongsToSky) {
+            return; // point does not belong to a plane
+        }
+
+        const {px, py, pz} = this.getPointDirection(x, y, w, h);
+
+        let plane: { normal: [number, number, number], distance: number } = depthData.planes[planeID];
+        let t = plane.distance / (px * plane.normal[0] + py * plane.normal[1] + pz * plane.normal[2]);
+
+        // size correction based on orientation:
+        // 1 / cos (angle between normal and ray to point) = 1 / dot product (normal, ray to point)
+        const orientationCorrection = 1 / Math.abs(px * plane.normal[0] + py * plane.normal[1] + pz * plane.normal[2]);
+        const distanceCorrection = Math.abs(t) + 0.001;
+        const size = splatScale * distanceCorrection * orientationCorrection;
+        if (size > this.options.maxNonSkySplatSize) {
+            return; // discard points that are too large
+        }
+
+        if (size < this.options.minNonSkySplatSize) {
+            // try merging points
+            let maxSize = Math.ceil(this.options.minNonSkySplatSize / size);
+            maxSize = Math.min(maxSize, h - y);
+            maxSize = Math.min(maxSize, w - x);
+            for (let dx = 0; dx < maxSize; dx++) {
+                for (let dy = 0; dy < maxSize; dy++) {
+                    if (depthData.indices[(y+dy) * w + (x+dx)] !== planeID) {
+                        // point within the box has a different plane -> reduce box
+                        maxSize = Math.min(maxSize, dx);
+                        maxSize = Math.min(maxSize, dy);
+                    }
+                }
+            }
+            // max size of the box now known
+            if (maxSize > 1) {
+                // merge all points within the box into a single point
+
+
+            }
+        }
+
+
+        // color: map x and y to pixel position (depends on effective image size)
+        const {r, g, b} = this.getPointColor(x, y, w, h, imageWidth, imageHeight, pixels);
+
+        output.positions.push(px * t, py * t, pz * t);
+        output.sizes.push(size);
+        output.colors.push(r, g, b);
+        output.normals.push(-plane.normal[0], -plane.normal[1], -plane.normal[2]);
 
     }
 
