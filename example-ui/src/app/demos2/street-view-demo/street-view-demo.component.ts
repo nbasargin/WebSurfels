8c6897a4e99a03623cc855397813a804d5b95f62
Nikita Basargin <nbasargin@web.de>
2020-07-02 00:13:52 +0200

    orbit animation in street view

--- example-ui/src/app/demos2/street-view-demo/street-view-demo.component.ts
+++ example-ui/src/app/demos2/street-view-demo/street-view-demo.component.ts
@@ -3,10 +3,7 @@ import { FormControl } from '@angular/forms';
 import { MatSelectChange } from '@angular/material/select';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { vec3 } from 'web-surfels';
 import { environment } from '../../../environments/environment';
-import { Benchmark } from '../../benchmarks/benchmark';
-import { CameraPath } from '../../benchmarks/camera-path';
 import { RendererService } from '../../services/renderer.service';
 
 import { DynamicStreetViewController, LocalStreetViewApi, Renderer, StreetViewLoader } from 'web-surfels';
@@ -88,6 +85,24 @@ import { DynamicStreetViewController, LocalStreetViewApi, Renderer, StreetViewLo
             </div>
         </mat-expansion-panel>
 
+        <mat-expansion-panel *ngIf="!controller.errorLoadingRoot" [expanded]="true">
+            <mat-expansion-panel-header>
+                <mat-panel-title>Controller</mat-panel-title>
+            </mat-expansion-panel-header>
+
+            <mat-radio-group [formControl]="controlModeControl" (change)="rendererService.setControlMode($event.value)">
+                <mat-radio-button value="first-person">First-person (WASD)</mat-radio-button><br>
+                <mat-radio-button value="orbit-animation">Orbit animation</mat-radio-button>
+            </mat-radio-group>
+
+            <div *ngIf="controlModeControl.value === 'first-person'" style="margin-top: 5px">
+                Movement speed: {{rendererService.getMovementSpeed().toLocaleString('en-us')}}<br>
+                <span style="color: gray">
+                    Change movement speed with the mouse scroll wheel. 
+                </span>
+            </div>
+        </mat-expansion-panel>
+
         <mat-expansion-panel *ngIf="controller.hasNetworkErrors()" [expanded]="true">
             <mat-expansion-panel-header>
                 <mat-panel-title>Network Errors!</mat-panel-title>
@@ -113,7 +128,6 @@ export class StreetViewDemoComponent implements OnDestroy {
     controller: DynamicStreetViewController;
     moreAboutPointLoading = false;
 
-    benchmark: Benchmark;
     datasets = {
         paris25k: {
             path: 'paris25k',
@@ -140,15 +154,17 @@ export class StreetViewDemoComponent implements OnDestroy {
         {text: 'Extra High', min: 100e6, max: 120e6},
     ];
     memoryLimitControl = new FormControl();
+    controlModeControl = new FormControl();
 
-    constructor(private rendererService: RendererService) {
+    constructor(public rendererService: RendererService) {
         this.rendererService.setFpsAveragingWindow(20);
-        this.rendererService.setControlMode('first-person');
+        this.rendererService.setControlMode('orbit-animation');
+        this.controlModeControl.patchValue('orbit-animation');
         this.rendererService.setMovementSpeed(0.5);
 
         this.renderer = this.rendererService.getRenderer();
         this.renderer.camera.setClippingDist(0.1, 10000);
-        this.renderer.camera.setOrientation([169.29, -114.45, 50.61], [103.97, -79.77, -16.68], [0, 1, 0]);
+        this.rendererService.setOrbitAnimation(30, 100, 30, 50000);
 
         this.renderer.light.ambientIntensity = 1;
 
@@ -163,10 +179,7 @@ export class StreetViewDemoComponent implements OnDestroy {
             softMaximum: 50_000_000
         }, data.startID);
 
-        this.setUpBenchmark();
-
-        this.rendererService.nextFrame.pipe(takeUntil(this.destroyed$)).subscribe((msPassed) => {
-            this.rendererService.setControlMode(this.benchmark.running ? 'disabled' : 'first-person');
+        this.rendererService.nextFrame.pipe(takeUntil(this.destroyed$)).subscribe(() => {
             this.controller.render();
         });
 
@@ -190,44 +203,6 @@ export class StreetViewDemoComponent implements OnDestroy {
         const selectedLimit = this.memoryLimits.find(l => l.text === e.value) || this.memoryLimits[0];
         this.controller.pointLoadingBudgets.softMinimum = selectedLimit.min;
         this.controller.pointLoadingBudgets.softMaximum = selectedLimit.max;
-    }
-
-    private setUpBenchmark() {
-        const cameraPath = new CameraPath(this.renderer.camera, [
-            {
-                pos: vec3.fromValues(1240.0255126953125, 428.3453369140625, -51.90530776977539),
-                viewDirection: vec3.fromValues(-0.9269274473190308, -0.3198262155056, -0.19625674188137054)
-            },
-            {
-                pos: vec3.fromValues(389.5700378417969, 8.811860084533691, -333.1141052246094),
-                viewDirection: vec3.fromValues(-0.5020412802696228, -0.8314081430435181, 0.23814919590950012)
-            },
-            {
-                pos: vec3.fromValues(329.5191650390625, -384.33477783203125, -177.86441040039062),
-                viewDirection: vec3.fromValues(-0.7333635091781616, 0.6777465343475342, -0.053268913179636)
-            },
-            {
-                pos: vec3.fromValues(-180.10511779785156, -241.0279083251953, 211.83261108398438),
-                viewDirection: vec3.fromValues(-0.01191917434334755, 0.8558781147003174, -0.5170401930809021)
-            },
-            {
-                pos: vec3.fromValues(-259.0425720214844, -459.4671630859375, 533.74365234375),
-                viewDirection: vec3.fromValues(0.06708746403455734, 0.5258428454399109, -0.8479319214820862)
-            },
-            {
-                pos: vec3.fromValues(-360.62603759765625, -110.98513793945312, 386.25799560546875),
-                viewDirection: vec3.fromValues(0.15893428027629852, 0.36665815114974976, -0.9166797399520874)
-            },
-            {
-                pos: vec3.fromValues(-270.24658203125, 123.03898620605469, 272.4951171875),
-                viewDirection: vec3.fromValues(-0.25811368227005005, -0.8686648607254028, -0.42284587025642395)
-            },
-            {
-                pos: vec3.fromValues(-124.8250961303711, 573.46044921875, 117.2672119140625),
-                viewDirection: vec3.fromValues(-0.9989715218544006, -0.04266732931137085, 0.015343156643211842)
-            },
-        ]);
-        this.benchmark = new Benchmark(cameraPath);
     }
 
 }
